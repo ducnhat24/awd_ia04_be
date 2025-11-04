e65a55f7f1072620f2707163e48a179d77898844
ducnhat24 <ducnhaat@gmail.com>
2025-11-05 00:17:28 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+      "https://awd-ia04-fe.vercel.app"
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
